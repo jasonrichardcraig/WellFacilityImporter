b23242fb8d6a620831b4dc25880fa211d0e2737d
Jason Craig <jason.craig@outlook.com>
2024-10-22 08:55:00 -0600

    Fixed project rename problem

---
+++ WellFacilityImporter/WellFacilityClrUtilities/Functions/CamelCaseString.cs
@@ -0,0 +1,44 @@
+using Microsoft.SqlServer.Server;
+using System.Data.SqlTypes;
+using System.Linq;
+
+public partial class UserDefinedFunctions
+{
+    [SqlFunction]
+    public static SqlString CamelCaseString(SqlString input)
+    {
+        if (input.IsNull || input.Value.Length == 0)
+            return SqlString.Null;
+
+        string strInput = input.Value;
+        bool capitalizeNext = true;
+
+        var result = new string(
+            strInput
+            .Select((c, i) =>
+            {
+                if (c == '-' || c == '\'')
+                {
+                    capitalizeNext = true;
+                    return c;
+                }
+                if (c == ' ')
+                {
+                    capitalizeNext = true;
+                    return c;
+                }
+
+                if (capitalizeNext)
+                {
+                    capitalizeNext = false;
+                    return char.ToUpper(c);
+                }
+                else
+                {
+                    return char.ToLower(c);
+                }
+            }).ToArray());
+
+        return new SqlString(result);
+    }
+}
---
+++ WellFacilityImporter/WellFacilityClrUtilities/Functions/ConvertFacilityCode.cs
@@ -0,0 +1,22 @@
+using Microsoft.SqlServer.Server;
+using System.Data.SqlTypes;
+
+public partial class UserDefinedFunctions
+{
+    [SqlFunction]
+    public static SqlString ConvertFacilityCode(SqlString input)
+    {
+        if (input.IsNull || input.Value.Length < 2)
+        {
+            return SqlString.Null;
+        }
+
+        // Remove the leading character and extract the numeric part
+        string numericPart = input.Value.Substring(1);
+
+        // Pad the numeric part with leading zeros to ensure it's 7 characters long
+        string result = numericPart.PadLeft(7, '0');
+
+        return new SqlString(result);
+    }
+}
